fff0253e1231755bde5da8272eaa2b7197abd674
Abel Zazueta <abel.zazueta@gmail.com>
2022-05-26 15:05:35 -0700

    Tests passed

---
+++ src/components/Article.js
@@ -0,0 +1,17 @@
+import React from "react";
+import logo from "../assets/Coffee_Outline.svg"
+function Article({ id, title, date = "January 1, 1970", preview, minutes }) {
+    const amountOfCoffees=Math.round(minutes / 5);
+    let coffees=[];
+    for (let i =0; i<amountOfCoffees; i++){
+        coffees.push(<img src={logo} alt="time"></img>);
+    }    
+    return (
+        <article>
+            <h3>{title}</h3>
+            <small>{date}{coffees}{minutes} min read</small>
+            <p>{preview}</p>
+        </article>
+    );
+}
+export default Article;
